ed92544d862fb7a94e08d76ec2d4da3ee871d49c
Rashad Hasan <rashadhasandev@gmail.com>
2024-07-18 14:00:23 +0400

    delete translate function

--- src/app.js
+++ src/app.js
@@ -32,23 +32,3 @@ async function generateJoke() {
 
   jokeEl.innerText = data.joke;
 }
-
-async function translateToTurkish(text) {
-  const apiKey = "YOUR_GOOGLE_TRANSLATE_API_KEY";
-  const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
-  const body = {
-    q: text,
-    target: "tr",
-  };
-
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-
-  const data = await res.json();
-  return data.data.translations[0].translatedText;
-}
